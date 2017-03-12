6bab9078f8beabcf5b68ab71af017897b64fcfba
Yuuki Wesp <ls-micro@ya.ru>
2017-03-12 13:25:04 +0300

    [vm] updated core engine

--- FlameVM/Core/EngineVM.cs
+++ FlameVM/Core/EngineVM.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using Flame.API.Core;
 #pragma warning disable 1998
 using System.Threading.Tasks;
 using EdgeJs;
@@ -14,22 +15,44 @@ namespace FlameVM.Core
 {
     public class EngineVM
     {
+        private static string HeaderNodeJS = @"
+            let Log = exports.flameVM.Engine.Log;
+            let Error = exports.flameVM.Engine.Error;
+            let Warning = exports.flameVM.Engine.Warning;";
+
+
         private libxFile compilePacket;
         private string mainTS;
         private readonly Dictionary<string, object> contextDictionary = new Dictionary<string, object>();
         private readonly Engine jsEngine;
-        private string compiledJS;
-        public EngineVM()
+        public EngineVM(FlameXConfig config)
         {
-            jsEngine = new Engine(cfg => cfg.AllowClr());
+            jsEngine = new Engine(cfg =>
+            {
+                if (config.Is("harmony"))
+                    cfg.AllowClr();
+            });
 
 
 
             Edge.Func(@"
+            var Flame;
+            (function (Flame) {
+                var Engine = (function () 
+                {
+                    function Engine() {}
+                    Engine.Log = function (s) { exports.flameVM.VMLog(s); };
+                    Engine.Error = function (s) { exports.flameVM.VMError(s); };
+                    Engine.Warning = function (s) { exports.flameVM.VMWarning(s); };
+                    return Engine;
+                }());
+                Flame.Engine = Engine;
+            })(Flame || (Flame = {}));
             return function(data, callback) 
             {
                 exports.flameVM = {};
                 exports.flameVM.methods = [];
+                exports.flameVM.Engine = Flame.Engine;
                 callback(null, null);
             }  
             ").Invoke(null).Wait();
@@ -38,6 +61,8 @@ namespace FlameVM.Core
             BindContext<Action<string>>("VMLog", Log);
             BindContext<Action<string>>("VMWarn", Waring);
             BindContext<Action<string>>("VMError", Error);
+
+            BindContext<Func<string>>(nameof(VMGetVersion), VMGetVersion);
 
 
             jsEngine.Execute(@"
@@ -54,8 +79,14 @@ namespace FlameVM.Core
                 Flame.Engine = Engine;
             })(Flame || (Flame = {}));");
             jsEngine.Execute("var exports = {}; exports.__esModule = false;");
-            jsEngine.Execute("Flame.Engine.Log('VM Started');");
+            jsEngine.Execute("Flame.Engine.Log('[VM] Initialization is complete.');");
         }
+
+        public void BindContext<T>(T t)
+        {
+            this.BindContext(nameof(T), t);
+        }
+
         public void BindContext<T>(string key, T obj)
         {
             if (contextDictionary.ContainsKey(key))
@@ -66,12 +97,11 @@ namespace FlameVM.Core
             contextDictionary.Add(key, obj);
             jsEngine.SetValue(key, obj);
 
-            if (typeof(T) == typeof(Action<string>))
-            {
-                Action<string> s = (Action<string>)(object)obj;
-                s("test in engine");
-                this.BindDelegate<string>(s, key);
-            }
+            if (typeof(T) == typeof(Action<string>))  this.BindDelegate((Action<string>)(object)obj, key);
+            if (typeof(T) == typeof(Action<int>))     this.BindDelegate((Action<int>)   (object)obj, key);
+            if (typeof(T) == typeof(Action<bool>))    this.BindDelegate((Action<bool>)  (object)obj, key);
+            if (typeof(T) == typeof(Action<object>))  this.BindDelegate((Action<object>)(object)obj, key);
+            if (typeof(T) == typeof(Action<byte>))    this.BindDelegate((Action<byte>)  (object)obj, key);
         }
 
         private void BindDelegate<T>(Action<T> a, string name)
@@ -83,12 +113,13 @@ namespace FlameVM.Core
             });
 
             Edge.Func($"return function(data, callback)" +
-           "{" +
+            "{" +
            $"    exports.flameVM.{name} = data;" +
            $"    callback(null, null);" +
-           "}").Invoke(wrapFucn).Wait();
+            "}").Invoke(wrapFucn).Wait();
         }
 
+        public static Func<object, Task<object>> Execute(string code) { return Edge.Func(HeaderNodeJS + code); }
         public void SetEngine(libxFile pack) { this.compilePacket = pack; }
         public void BindMain(string path)
         {
@@ -107,20 +138,20 @@ namespace FlameVM.Core
                 return null;
             });
 
-            var qwe0 = Edge.Func(@"
-            return function(data, callback) 
+            var qwe0 = EngineVM.Execute(
+            @"return function(data, callback) 
             {
-                exports.flameVM.VMLog('sqe0 execute!');
+                Log('sqe0 execute!');
                 exports.nure = 1;
 
                 data(exports.nure++, true);
                 callback(null, null);
             }  
             ");
-            var qwe1 = Edge.Func(@"
+            var qwe1 = EngineVM.Execute(@"
             return function(data, callback) 
             {
-                exports.flameVM.VMLog('sqe1 execute!');
+                Log('sqe1 execute!');
                 data(exports.nure++, true);
                 callback(null, null);
             }  
@@ -132,7 +163,7 @@ namespace FlameVM.Core
             qwe1(sq).Wait();
             qwe1(sq).Wait();
 
-            var func = Edge.Func(@"
+            var func = EngineVM.Execute(@"
             let ts = require('typescript');
             let TCSResult = '';
             let compilerOptions = { module: ts.ModuleKind.None, removeComments: true };
@@ -161,6 +192,8 @@ namespace FlameVM.Core
             //}
         }
 
+
+        public static string VMGetVersion() => "Node 7.1, V8 ES5, .NET 4.6.2, RC.Core 9.2, Flame.API 1.2, FlameVM 0.4";
         public static void Log(string s)
         {
             Terminal.WriteLine($"[{RCL.Wrap("VM", ConsoleColor.DarkMagenta)}][{RCL.Wrap("LOG", ConsoleColor.DarkGray)}]: {s}");

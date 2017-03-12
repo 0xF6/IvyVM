1e4788829724d2c3a7fe1d3c670dd076979ae692
Yuuki Wesp <ls-micro@ya.ru>
2017-03-12 13:25:24 +0300

    [core] fix load config

--- FlameVM/Program.cs
+++ FlameVM/Program.cs
@@ -1,7 +1,8 @@
 ﻿using System;
+using System.IO;
 using System.Threading;
+using Flame.API.Core;
 using FlameVM.Core;
-using Noesis.Javascript;
 
 namespace FlameVM
 {
@@ -11,9 +12,11 @@ namespace FlameVM
         {
             Console.Title = "Flame VM - Context: [TypeScript]";
 
+            FlameXConfig conf = FlameXConfig.Parse(File.ReadAllText("config\\vm.fx"));
+
             Thread.Sleep(2000);
 
-            EngineVM VM = new EngineVM();
+            EngineVM VM = new EngineVM(conf);
 
             VM.SetEngine(libxFile.LoadFromFile("lib\\tsc.lib"));
             VM.BindMain("main.ts");
